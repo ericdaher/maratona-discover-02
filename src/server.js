9b32ebe6f2d09a8c4088b1af3a37cbcf21ca3f79
ericdaher <ericdaher2@gmail.com>
2021-03-30 23:34:24 -0300

    crud operations done

--- src/server.js
+++ src/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 
+//template engine
 server.set("view engine", "ejs");
 
 server.use(express.static("public"));
+
+// req body
+server.use(express.urlencoded({ extended: true }));
 
 server.use(routes);
 server.listen(3000, () => console.log("running"));
